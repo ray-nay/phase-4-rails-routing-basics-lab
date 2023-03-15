f5b3a508d5cc909c82346586c26eddfe6fde7645
Renee Mumbi <1723renee@gmail.com>
2023-03-15 15:06:24 +0300

    Done.2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,12 @@ class StudentsController < ApplicationController
         students = Student.all
         render json: students
     end 
+    def grades
+        studentgrades = Student.order(grade: :desc)
+        render json: studentgrades
+    end 
+    def highest_grade
+        highest = Student.order(grade: :desc).first
+        render json: highest
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   get 'students', to: 'students#index'
 
+  get 'students/grades', to: 'students#grades'
+
+  get 'students/highest-grade', to: 'students#highest_grade'
+
+
 end
